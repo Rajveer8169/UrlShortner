09dc2a8dad4a7816d31846be00baeaa04b24cf54
Rajveer8169 <rajveerrai098@gmail.com>
2026-07-17 17:43:36 +0530

    fixed url

--- FRONTEND/src/api/shortUrl.api.js
+++ FRONTEND/src/api/shortUrl.api.js
@@ -1,6 +1,6 @@
 import axiosinstance from "../utils/axiosinstance";
 
 export const createShortUrl = async(url,slug) =>{
-    const {data} = await axiosinstance.post("http://localhost:3000/api/create", {url,slug});
+    const {data} = await axiosinstance.post("/api/create", {url,slug});
     return data.shortUrl;
 }
